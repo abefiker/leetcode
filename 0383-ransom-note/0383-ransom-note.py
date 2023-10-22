5efafad98245467545788be3910bd3c9a7c8f770
Abemelek <111175442+abefiker@users.noreply.github.com>
2023-10-22 22:41:29 +0300

    Time: 52 ms (81.01%), Space: 16.6 MB (15.21%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -1,12 +1,10 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        ransomNote_count = {}
-        magazine_count = {}
+        ransomNote = list(ransomNote)
+        magazine = list(magazine)
         for char in ransomNote:
-            ransomNote_count[char] = ransomNote_count.get(char , 0) + 1
-        for char in magazine:
-            magazine_count[char] = magazine_count.get(char , 0) + 1   
-        for char , count in ransomNote_count.items():
-            if magazine_count.get(char , 0 ) < count:
-                return False
+            if char in magazine:
+              magazine.remove(char)
+            else:    
+              return False
         return True        
